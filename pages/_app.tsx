2943bec5b6cb77bb8e746cd518fddb7c90ce852c
chisom promise <chimisepro@gmail.com>
2023-01-11 09:07:46 +0100

    feat: Configure layouts in the root component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
+import React from 'react';
 import type { AppProps } from 'next/app'
+import { NextPage } from 'next';
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type Component = {
+  getLayout?: (children: React.ReactNode) => JSX.Element;
+} & NextPage;
+
+type App = {
+  Component: Component
+} & AppProps;
+
+export default function App({ Component, pageProps }: App) {
+  const defaultLayout = (children: React.ReactNode) => children;
+  const getLayout = Component.getLayout || defaultLayout;
+  return getLayout(<Component {...pageProps} />);
 }
