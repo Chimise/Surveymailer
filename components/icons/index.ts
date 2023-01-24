5f691715dfa12c1b9860fef6167f1529d7521b8c
chisom promise <chimisepro@gmail.com>
2023-01-24 10:28:43 +0100

    feat: Add new custom icons

---
+++ components/icons/index.ts
@@ -0,0 +1,5 @@
+export interface IconProps extends React.SVGAttributes<SVGSVGElement> {}
+
+export {default as GoogleIcon} from './GoogleIcon';
+export {default as TriangleIcon} from './TriangleIcon';
+export {default as GridIcon} from './GridIcon';
