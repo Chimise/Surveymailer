c7c09b205c629cba7ccd65b974675f017616193b
chisom promise <chimisepro@gmail.com>
2023-02-06 10:31:19 +0100

    fix(styles): Replace a button in the homepage with a link component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 import Image from 'next/image'
 import cn from 'classnames';
+import Link from 'next/link';
 import Layout from "../components/common/Layout";
 import Container from "../components/ui/Container";
 import Button from '../components/ui/Button';
@@ -36,7 +37,7 @@ const HomePage = () => {
           </div>
           <div className='flex flex-col sm:flex-row sm:justify-center space-y-3 sm:space-y-0 sm:space-x-3'>
               <Button variant="outlined">Learn More</Button>
-              <Button>Sign up for free</Button>
+              <Link href='/auth/register' className="text-center items-center justify-center rounded-md bg-secondary text-white hover:bg-green-700 px-4 py-2">Sign up for free</Link>
           </div>
           <p className="text-sm">No credit card required</p>
         </div>
